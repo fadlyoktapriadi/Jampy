43db0afffbd214cb5b703e1693fb15b7aea7d5d9
fadlyoktapriadi <fadlyoktapriadi41@gmail.com>
2024-12-09 16:37:32 +0700

    add card float

--- app/src/main/java/com/example/jampy/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/example/jampy/ui/screen/HomeScreen.kt
@@ -11,13 +11,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,6 +30,7 @@ import com.example.jampy.R
 import com.example.jampy.ui.theme.Outfit
 import com.example.jampy.ui.theme.primaryGreen
 import com.example.jampy.ui.theme.secondaryGreen
+import com.example.jampy.ui.theme.tersiaryGreen
 
 @Composable
 fun HomeScreen() {
@@ -61,7 +64,7 @@ fun HomeScreen() {
                         Image(
                             painter = painterResource(id = R.drawable.jampy),
                             contentDescription = "Logo application the name is Jampy",
-                            modifier = Modifier.size(50.dp)
+                            modifier = Modifier.size(40.dp)
                         )
 
                         Image(
@@ -87,29 +90,42 @@ fun HomeScreen() {
                 horizontalArrangement = Arrangement.Center,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(top = 24.dp, start = 24.dp, end = 24.dp)
-                    .shadow(3.dp, shape = RoundedCornerShape(20.dp))
-                    .background(
-                        color = primaryGreen,
-                        shape = RoundedCornerShape(20.dp)
-                    )
+                    .padding(top = 8.dp, start = 24.dp, end = 24.dp)
                     .constrainAs(profile) {
                         top.linkTo(topImag.bottom)
                         bottom.linkTo(topImag.bottom)
                         start.linkTo(parent.start)
                         end.linkTo(parent.end)
                     }
-//                    .padding(16.dp)
             ) {
-                Column(
-                    modifier = Modifier
-                        .padding(top = 16.dp, bottom = 12.dp, end = 12.dp)
-                        .background(
-                            color = secondaryGreen,
-                            shape = RoundedCornerShape(20.dp)
-                        )
-                ){
-
+                Box(
+                    contentAlignment = Alignment.Center
+                ) {
+                    Card(
+                        colors = CardDefaults.cardColors(
+                            containerColor = tersiaryGreen
+                        ),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .size(width = 200.dp, height = 130.dp)
+                            .graphicsLayer {
+                                rotationZ = 8f
+                            }
+                    ) {
+                    }
+                    Card(
+                        colors = CardDefaults.cardColors(
+                            containerColor = primaryGreen
+                        ),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .size(width = 200.dp, height = 130.dp)
+                            .graphicsLayer {
+                                rotationZ = 0f
+                            }
+                    ) {
+                        // Card 1 content
+                    }
                 }
             }
 
